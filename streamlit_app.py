bbeb6e25fe2878b85849f2a929c48fc5783ef415
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-02-10 14:40:29 +1100

    Update streamlit_app.py
    removing button

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,3 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 st.dataframe(my_fruit_list)
 
 
-
-
-if st.button("My Button"):
-    my_input = st.text_input("Input a text here")
-    _, center, _ = st.columns([3, 1, 3])
-    submit = center.button("Submit")
-    if submit:
-        st.write("You have entered: ", my_input)
