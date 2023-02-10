948c3bfd51190c91a02c0a0c4097ca41b541437b
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-02-10 14:18:09 +1100

    Update streamlit_app.py
    placing an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title('My parents new healthy diner')
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown(”This text is :red[colored red], and this is **:blue[colored]** and bold.”)
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+print this error 
