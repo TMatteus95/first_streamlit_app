aa1cd509e9e958e7ed102fab0bb2aed4c8788798
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-13 21:34:39 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,5 +75,6 @@ if streamlit.button('Add a fruit to the list'):
     streamlit.text(back_from_function)
 
 
-m = folium.Map(location=[57.708870, 11.974560])
+m = folium.Map(location=[57.708870, 11.974560], zoom_start=1)
 st_data = st_folium(m, width=700)
+
