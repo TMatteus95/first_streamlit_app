b4c3087a9182caccbb361f6e2510409af2366f67
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-13 21:58:46 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,7 @@ tooltip = "Click me!"
 folium.Marker(
     [56.260860, 12.550790], popup="<p>Holy Smoke BBQ</p> <p>Recesent: Dagen nyheter  Betyg: 5/5 L&auml;nk: https://www.dn.se/kultur/holy-smoke-skansk-barbecue-pa-riktigt/</p>", tooltip=tooltip
 ).add_to(m)
+folium.TileLayer('cartodbdark_matter').add_to(m)
 
 st_data = st_folium(m, width=700)
 
