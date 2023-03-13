b285889780a66aed5c9d9f2200a92f7fe635f438
TMatteus95 <61378788+TMatteus95@users.noreply.github.com>
2023-03-13 21:47:08 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,17 @@ from urllib.error import URLError
 import folium
 from streamlit_folium import st_folium, folium_static
 
+
+
+m = folium.Map(location=[57.708870, 11.974560], zoom_start=6)
+st_data = st_folium(m, width=700)
+folium.Marker(
+    [56.260860, 12.550790], popup="<p>Holy Smoke BBQ</p><p>Recesent: Dagen nyheter  Betyg: 5/5 L&auml;nk: https://www.dn.se/kultur/holy-smoke-skansk-barbecue-pa-riktigt/</p>", tooltip=tooltip
+).add_to(m)
+
+
+
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -75,6 +86,5 @@ if streamlit.button('Add a fruit to the list'):
     streamlit.text(back_from_function)
 
 
-m = folium.Map(location=[57.708870, 11.974560], zoom_start=6)
-st_data = st_folium(m, width=700)
+
 
